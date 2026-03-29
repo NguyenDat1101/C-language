2c866ac7d8efc19966188a3d6c314d2a1228a371
NguyenDat-Vanilla <nguyenminhdat160407@gmail.com>
2026-03-29 22:29:34 +0700

    Code C

---
+++ C0015.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct SinhVien
+{
+  char hoten[30];
+  char mssv[10];
+  int namsinh;
+  float diem;
+}sv;
+
+void nhapsinhvien (struct SinhVien *sv)
+{
+
+  printf("nhap ten sinh vien:");
+    fgets(sv->hoten, sizeof(sv->hoten), stdin);
+    sv->hoten[strcspn(sv->hoten, "\n")] = 0;
+
+  printf("nhap mssv:");
+    fgets(sv->mssv, sizeof(sv->mssv), stdin);
+    sv->mssv[strcspn(sv->mssv, "\n")] = 0;
+  
+nhaplai:
+  printf("nhap nam sinh:");
+    scanf("%d", &sv->namsinh);
+      if (sv->namsinh < 1900 || sv->namsinh > 2026)
+      {
+        printf("\nnam sinh khong hop le\n");
+          goto nhaplai;
+      }
+nhaplai1:
+  printf("nhap diem:");
+    scanf("%f", &sv->diem);
+      if(sv->diem < 0 || sv->diem > 4)
+      {
+        printf("\ndiem khong hop le\n");
+        goto nhaplai1;
+      }
+  getchar();
+}
+
+void nhapnhieusinhvien(int n, struct SinhVien sv[])
+{
+  for (int i=0; i<n; i++)
+  {
+    nhapsinhvien(&sv[i]);
+  }
+}
+
+void xuatsinhvien(struct SinhVien sv[], int n)
+{
+  printf("*****************************************\n");
+  printf("* Ho va Ten || nam sinh || mssv || diem *\n");
+
+  for(int i=0; i<n; i++)
+  {
+  printf("%s\t%d\t%s\t%.2f\n", 
+      sv[i].hoten, 
+      sv[i].namsinh, 
+      sv[i].mssv, 
+      sv[i].diem);
+  }
+}
+
+int main()
+{
+  int n;
+  printf("\nnhap so luong sinh vien:");
+    scanf("%d", &n);
+
+  getchar();
+
+  struct SinhVien sv[100];
+  nhapnhieusinhvien(n, sv);
+  xuatsinhvien(sv, n);
+
+return 0;
+}
